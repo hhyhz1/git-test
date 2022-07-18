f299745f25fdab6c92efe87d81d6901372fdab6e
ddatou <ddatou@qq.com>
2022-07-18 18:17:11 +0800

    first commit

---
+++ src/main/java/com/aatguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.aatguigu.git;
+
+/**
+ * @ClassName Git
+ * @Description
+ * @Author DaTou
+ * @Date 2022/7/18 18:09
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("dududu");
+    }
+}
